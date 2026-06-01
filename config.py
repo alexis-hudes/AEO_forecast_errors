667847050003de1161eb384ade48b9ee73436fa8
Alexis Hudes <alexis.hudes.th@dartmouth.edu>
2026-06-01 17:31:33 -0400

    improving commenting

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ API_KEY = 'your-key-here'
 # What to analyze
 fuel    = "Natural Gas"      # Natural Gas | Electricity | Propane | Distillate Fuel
 sector  = "Residential"      # Electric Power | Commercial | Residential | Industrial
-region  = "New England"    # United States | New England
-value   = "real"             # real | nom
+region  = "New England"      # United States | New England
+value   = "real"             # real | nom (not fully set-up to work with nom yet)
 
 # Time settings
 current_year     = 2026
